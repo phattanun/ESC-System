1b90466e441bb4a0d2a1426aa7fe51d3412b448e
Phattanun <phattanun.auk@gmail.com>
2016-01-25 23:23:57 +0700

    masterpage and blog mockup

---
+++ public/assets/js/blog.js
@@ -0,0 +1,4 @@
+$(".tab-button").click(function(){
+	$(".tab-button").removeClass("action");
+    $(this).addClass("action");
+});
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,91 @@
+@extends('masterpage')
+
+@section('title')
+    ประกาศข่าวสาร
+@endsection
+@section('body-attribute')
+
+@endsection
+@section('newsNavToggle')
+    active
+@endsection
+@section('content')
+    <div class="margin-bottom-60"></div>
+    <div class="blog-container">
+        <div class="blog-all">
+            <div class="blog-box" style="background-image:url(assets/images/backgrounds/blue.jpg);">
+                <img src="img/blog/cover.jpg" alt="" />
+                <div class="blog-box-card">
+                    <div class="row blog-box-head">
+                        <h2>ชื่อเรื่อง</h2>
+                    </div>
+                    <div class="row blog-box-content">
+                        เนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหา
+                    </div>
+                    <div class="row blog-box-foot">
+                        <div>1 days ago&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;150 Views</div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="blog-box" style="background-image:url(assets/images/backgrounds/bg2-min.jpg);">
+                <img src="img/blog/cover.jpg" alt="" />
+                <div class="blog-box-card">
+                    <div class="row blog-box-head">
+                        <h2>ชื่อเรื่อง</h2>
+                    </div>
+                    <div class="row blog-box-content">
+                        เนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหา
+                    </div>
+                    <div class="row blog-box-foot">
+                        <div>1 days ago&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;150 Views</div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="blog-box" style="background-image:url(assets/images/backgrounds/default.jpg);">
+                <img src="img/blog/cover.jpg" alt="" />
+                <div class="blog-box-card">
+                    <div class="row blog-box-head">
+                        <h2>ชื่อเรื่อง</h2>
+                    </div>
+                    <div class="row blog-box-content">
+                        เนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหาเนื้อหา
+                    </div>
+                    <div class="row blog-box-foot">
+                        <div>1 days ago&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;150 Views</div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="pagination-nav margin-bottom-60">
+            <ul>
+                <li><a href="#"><i class="fa fa-angle-left"></i></a></li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">6</a></li>
+                <li><a href="#">7</a></li>
+                <li><a href="#">8</a></li>
+                <li><a href="#">9</a></li>
+                <li><a href="#">10</a></li>
+                <li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+            </ul>
+        </div>
+    </div>
+@endsection
+
+@section('css')
+    <link href="assets/css/blog.css" rel="stylesheet" type="text/css" />
+@endsection
+
+@section('js')
+    <script src="assets/js/blog.js" type="text/javascript"></script>
+@endsection
