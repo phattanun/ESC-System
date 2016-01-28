66040b8533537c7ac41c34164ca2d2a727d2305b
Mongkud Klungpornkun <mongkud167@gmail.com>
2016-01-28 21:44:13 +0700

    Users DB

--- database/migrations/2016_01_28_121352_create_users_table.php
+++ database/migrations/2016_01_28_121352_create_users_table.php
@@ -13,12 +13,26 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->string('student_id')->primary();
+            $table->string('password');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('surname');
+            $table->string('nickname');
+            $table->string('address');
+            $table->timestamp('birthdate');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->string('facebook_link');
+            $table->string('line_id');
+            $table->string('emergency_contact');
+            $table->integer('department_id');
+            $table->char('group',1);
+            $table->string('allergy');
+            $table->string('anomaly'); # AKA. disease
+            $table->string('religion');
+            $table->char('blood_type',2);
+            $table->char('clothing_size',5);
+
         });
     }
 
