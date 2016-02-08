66206b2f1f3b9a757037dc2e9b7e64d2b6b9476b
Patipol Chiammunchit <Patipol Chiammunchit>
2016-02-08 10:52:33 +0700

    Change databaseSeeder code (add second user with no special permission)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use \App\User;
 use \App\Permission;
+use \App\Setting;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
          $this->call(UserTableSeeder::class);
          $this->call(news_seeder::class);
          $this->call(PermissionTableSeeder::class);
+         $this->call(SettingTableSeeder::class);
 
         Model::reguard();
     }
@@ -46,6 +48,25 @@ class UserTableSeeder extends Seeder
             'religion'=>'พุทธ',
             'blood_type'=>'A',
             'clothing_size'=>'L']);
+        User::create(['student_id'=>'2',
+            'password'=>bcrypt('2'),
+            'name'=>'ปฏิพล',
+            'surname'=>'เจียมมั่นจิต',
+            'nickname'=>'คุณแอดมิน',
+            'address'=>'29/50 หมู่บ้านอลิชา 1 พุทธบูชา 36 บางมด ทุ่งครุ กรุงเทพฯ 10140',
+            'birthdate'=>'1994-11-29',
+            'phone_number'=>'0924587067',
+            'email'=>'fongac127@gmail.com',
+            'facebook_link'=>'www.facebook.com/fonggag/',
+            'line_id'=>'fong127',
+            'emergency_contact'=>'0924587067',
+            'department'=>'คอมฯ ณ ห้องเขียว',
+            'group'=>'F',
+            'allergy'=>'ถั่ว',
+            'anomaly'=>'อะไรวะ',
+            'religion'=>'พุทธ',
+            'blood_type'=>'A',
+            'clothing_size'=>'L']);
     }
 }
 
@@ -53,8 +74,14 @@ class PermissionTableSeeder extends Seeder
 {
     public function run()
     {
-        Permission::create(['student_id'=>'1','permission'=>'room']);
-        Permission::create(['student_id'=>'1','permission'=>'news']);
-        Permission::create(['student_id'=>'1','permission'=>'admin']);
+        Permission::create(['student_id'=>'1','news'=>true,'room'=>true,'supplies'=>true,'activities'=>true,'student'=>true]);
+        Permission::create(['student_id'=>'2','news'=>false,'room'=>false,'supplies'=>false,'activities'=>false,'student'=>false]);
+    }
+}
+class SettingTableSeeder extends Seeder
+{
+    public function run()
+    {
+        Setting::create(['admin_id'=>'1','year'=>2558]);
     }
 }
