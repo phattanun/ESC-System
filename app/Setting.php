b61d4e0f25f338ecd8a1b3ac56e8396f4491036e
Patipol Chiammunchit <Patipol Chiammunchit>
2016-02-08 10:48:20 +0700

    Create settings table (admin_id, year)

---
+++ app/Setting.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    protected $table = 'settings';
+    protected $primaryKey = 'admin_id';
+    public $timestamps = false;
+}
