4c6202d21a8d40e14d96587aec8fb702f0beb71d
Mongkud Klungpornkun <mongkud167@gmail.com>
2016-02-12 23:35:12 +0700

    Add ComposerServiceProvider & ViewComposer are for sharing contents to view function.

---
+++ app/Http/ViewComposers/GlobalComposer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+
+class GlobalComposer {
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $view->with('user', Auth::user());
+    }
+
+}
