9122804d697f7f1596b4c120f77932a94b6197f4
Patipol Chiammunchit <Patipol Chiammunchit>
2016-02-13 00:11:28 +0700

    Add permission sending code to GlobalComposer class

--- app/Http/ViewComposers/GlobalComposer.php
+++ app/Http/ViewComposers/GlobalComposer.php
@@ -4,6 +4,8 @@ namespace App\Http\ViewComposers;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
+use \App\Permission;
+use \App\Setting;
 
 class GlobalComposer {
 
@@ -15,7 +17,18 @@ class GlobalComposer {
      */
     public function compose(View $view)
     {
-        $view->with('user', Auth::user());
+        $user = Auth::user();
+        if(is_null($user))
+            return null;
+        $permission_json = Permission::where('student_id',$user['student_id'])->select('room','news','supplies','activities','student')->first();
+        if($permission_json['news']) $user['news'] = true;
+        if($permission_json['room']) $user['room'] = true;
+        if($permission_json['supplies']) $user['supplies'] = true;
+        if($permission_json['activities']) $user['activities'] = true;
+        if($permission_json['student']) $user['student'] = true;
+        $isAdmin = Setting::where('admin_id',$user['student_id'])->first();
+        if(!is_null($isAdmin)) $user['admin'] = true;
+        $view->with('user', $user);
     }
 
 }
