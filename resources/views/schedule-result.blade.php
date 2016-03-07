0ba1807f880e587b26834f659bd4397d55ce4b24
Phattanun Aukkarapuntouch <Phattanun Aukkarapuntouch>
2016-03-07 18:00:35 +0700

    add view schedule result page

---
+++ resources/views/schedule-result.blade.php
@@ -0,0 +1,24 @@
+@extends('masterpage')
+
+@section('title')
+    ผลการจองห้องประชุม
+@endsection
+@section('body-attribute')
+
+@endsection
+@section('conferenceNavToggle')
+    active
+@endsection
+@section('bodyTitle')
+    ผลการจองห้องประชุม
+@endsection
+@section('content')
+
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('js')
+@endsection
