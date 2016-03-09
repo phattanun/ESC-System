cc7c28cde48349f4fa494da1146943940dfdc4d5
Phattanun Aukkarapuntouch <Phattanun Aukkarapuntouch>
2016-03-09 15:54:28 +0700

    add seeder but no data

---
+++ database/seeds/UserReservationSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserReservationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
