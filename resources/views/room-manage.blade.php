fedfaa9198d3b549cdfa6cc21b2a6092ffbf54e0
Krit <ac127_44264@hotmail.com>
2016-03-11 00:23:20 +0700

    create remove edit confirmremove Room in front-end ui finish

--- resources/views/room-manage.blade.php
+++ resources/views/room-manage.blade.php
@@ -13,6 +13,35 @@
 @endsection
 
 @section('content')
+
+    <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+
+                <!-- header modal -->
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="mySmallModalLabel">ต้องการลบห้องประชุมนี้ใช่หรือไม่ ?</h4>
+                </div>
+
+                <!-- body modal -->
+                <div class="modal-body">
+                    <div class="row text-center">
+                            <a id="confirm-remove-button" class="btn btn-3d btn-reveal btn-green" data-dismiss="modal">
+                                <i class="fa fa-check"></i>
+                                <span>ใช่</span>
+                            </a>
+                            <a class="btn btn-3d btn-reveal btn-red" data-dismiss="modal">
+                                <i class="fa fa-times"></i>
+                                <span>ไม่ใช่</span>
+                            </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
     <section style="margin-top: -40px">
         <div class="container" >
             <div class="panel panel-default">
@@ -59,13 +88,13 @@
                                 <label class="margin-bottom-20 ">เพิ่มห้องประชุม</label>
                                 <div class="fancy-form margin-bottom-20">
                                     <i class="fa fa-tag"></i>
-                                    <input type="text" class="form-control" placeholder="ชื่อห้องประชุม">
+                                    <input id="room-input-name-new" type="text" class="form-control" placeholder="ชื่อห้องประชุม">
                                 </div>
                                 <div class="fancy-form margin-bottom-20">
                                     <i class="fa fa-users"></i>
-                                    <input type="text" class="form-control" placeholder="จำนวนคนที่จุได้">
+                                    <input id="room-input-size-new" type="text" class="form-control" placeholder="จำนวนคนที่จุได้">
                                 </div>
-                                <a class="btn btn-3d btn-reveal btn-success pull-right">
+                                <a class="btn btn-3d btn-reveal btn-success pull-right" onclick="roomCreate()">
                                     <i class="fa fa-plus"></i>
                                     <span>เพิ่ม</span>
                                 </a>
@@ -89,67 +118,71 @@
                                                 <td>
                                                     <div id="room-name-1">ห้องประชุมใหญ่ 1</div>
                                                     <div id="room-input-name-1" class="hide">
-                                                        <input type="text" class="form-control" name="room[1][]" placeholder="ชื่อห้องประชุม" value="ห้องประชุมใหญ่1">
+                                                        <input id="room-input-name-box-1" type="text" class="form-control" name="room[1][]" placeholder="ชื่อห้องประชุม" value="ห้องประชุมใหญ่1">
                                                     </div>
                                                 </td>
                                                 <td>
                                                     <div id="room-size-1">30 คน</div>
                                                     <div id="room-input-size-1" class="hide">
                                                         <div class="col-xs-12 no-padding">
-                                                            <input type="text" class="form-control" style="display: inline; width: 90%;" name="room[1][]" placeholder="จำนวนคนที่จุได้" value="30">
+                                                            <input id="room-input-size-box-1" type="text" class="form-control" style="display: inline; width: 90%;" name="room[1][]" placeholder="จำนวนคนที่จุได้" value="30">
                                                             คน
                                                         </div>
                                                     </div>
                                                 </td>
 
 
-                                                <!--td id="room-name-1">ห้องประชุมใหญ่ 1</td>
-                                                <td id="room-size-1">30 คน</td>
-                                                <td id="room-input-name-1" class="hide"><input type="text" class="form-control" name="room[1][]" placeholder="ชื่อห้องประชุม" value="ห้องประชุมใหญ่1"></td>
-                                                <td id="room-input-size-1" class="hide"><div class="col-md-10 col-sm-10 no-padding"><input type="text" class="form-control" name="room[1][]" placeholder="จำนวนคนที่จุได้" value="30"></div><div class="col-md-2 col-sm-2" style="vertical-align:middle">คน</div></td-->
                                                 <td class="text-center">
-                                                    <a id="room-edit-button-1" onclick="roomEdit(1)" class="btn btn-3d btn-reveal btn-yellow room-edit-button">
+                                                    <a id="room-edit-button-1" onclick="roomEdit(1)" class="btn btn-3d btn-reveal btn-yellow">
                                                         <i class="fa fa-edit"></i>
                                                         <span>แก้ไข</span>
                                                     </a>
-                                                    <a id="room-remove-button-1" onclick="roomRemove(1)" class="room-remove-button delete-a-tuple social-icon social-icon-sm social-icon-round social-yelp" data-toggle="tooltip" data-placement="top" title="ลบจากสิทธิ์ทั้งหมด" style="vertical-align:middle">
+                                                    <a id="room-cancel-button-1" onclick="roomCancel(1)" class="btn btn-3d btn-reveal btn-red hide">
+                                                        <i class="fa fa-times"></i>
+                                                        <span>ยกเลิก</span>
+                                                    </a>
+                                                    <a id="room-remove-button-1" onclick="roomRemove(1)" class="delete-a-tuple social-icon social-icon-sm social-icon-round social-yelp" data-toggle="tooltip" data-placement="top" title="ลบห้องประชุมนี้" style="vertical-align:middle">
                                                         <i class="fa fa-minus"></i>
-                                                        <i class="fa fa-trash"></i>
+                                                        <i class="fa fa-trash" data-toggle="modal" data-target=".bs-example-modal-sm"></i>
                                                     </a>
                                                 </td>
                                             </tr>
-                                            <tr id="">
-                                                <input type="hidden" id="" name="room[1][]" value="ห้องประชุมใหญ่1">
-                                                <input type="hidden" id="" name="room[1][]" value="30">
-                                                <td class="margin-bottom-10"><input type="text"></td>
-                                                <td class="margin-bottom-10" contenteditable>30 คน</td>
+
+                                            <tr id="room-2">
+                                                <td>
+                                                    <div id="room-name-2">ห้องประชุมใหญ่ 2</div>
+                                                    <div id="room-input-name-2" class="hide">
+                                                        <input id="room-input-name-box-2" type="text" class="form-control" name="room[2][]" placeholder="ชื่อห้องประชุม" value="ห้องประชุมใหญ่2">
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div id="room-size-2">20 คน</div>
+                                                    <div id="room-input-size-2" class="hide">
+                                                        <div class="col-xs-12 no-padding">
+                                                            <input id="room-input-size-box-2" type="text" class="form-control" style="display: inline; width: 90%;" name="room[2][]" placeholder="จำนวนคนที่จุได้" value="20">
+                                                            คน
+                                                        </div>
+                                                    </div>
+                                                </td>
+
+
                                                 <td class="text-center">
-                                                    <a class="btn btn-3d btn-reveal btn-yellow">
+                                                    <a id="room-edit-button-2" onclick="roomEdit(2)" class="btn btn-3d btn-reveal btn-yellow">
                                                         <i class="fa fa-edit"></i>
                                                         <span>แก้ไข</span>
                                                     </a>
-                                                    <a id="" class="delete-a-tuple social-icon social-icon-sm social-icon-round social-yelp" data-toggle="tooltip" data-placement="top" title="ลบจากสิทธิ์ทั้งหมด" style="vertical-align:middle">
+                                                    <a id="room-cancel-button-2" onclick="roomCancel(2)" class="btn btn-3d btn-reveal btn-red hide">
+                                                        <i class="fa fa-times"></i>
+                                                        <span>ยกเลิก</span>
+                                                    </a>
+                                                    <a id="room-remove-button-2" onclick="roomRemove(2)" class="delete-a-tuple social-icon social-icon-sm social-icon-round social-yelp" data-toggle="tooltip" data-placement="top" title="ลบห้องประชุมนี้" style="vertical-align:middle">
                                                         <i class="fa fa-minus"></i>
-                                                        <i class="fa fa-trash"></i>
+                                                        <i class="fa fa-trash" data-toggle="modal" data-target=".bs-example-modal-sm"></i>
                                                     </a>
                                                 </td>
                                             </tr>
-                                            <!--tr id="">
-                                                <input type="hidden" id="" name="room[1][]" value="ห้องประชุมใหญ่1">
-                                                <input type="hidden" id="" name="room[1][]" value="30">
-                                                <td class="margin-bottom-10"><input type="text"></td>
-                                                <td class="margin-bottom-10" contenteditable>30 คน</td>
-                                                <td class="text-center">
-                                                    <a class="btn btn-3d btn-reveal btn-yellow">
-                                                        <i class="fa fa-edit"></i>
-                                                        <span>แก้ไข</span>
-                                                    </a>
-                                                    <a id="" class="delete-a-tuple social-icon social-icon-sm social-icon-round social-yelp" data-toggle="tooltip" data-placement="top" title="ลบจากสิทธิ์ทั้งหมด" style="vertical-align:middle">
-                                                        <i class="fa fa-minus"></i>
-                                                        <i class="fa fa-trash"></i>
-                                                    </a>
-                                                </td>
-                                            </tr-->
+
+
                                         </table>
                                     </div>
                                 </div>
@@ -324,16 +357,85 @@
         .hide{
             display: none;
         }
+        .btn{
+            width: 90px;
+        }
     </style>
 @endsection
 
 @section('js-top')
     <script>
+        var room_count = 2;
         function roomEdit(id){
             $("#room-name-"+id).addClass("hide");
             $("#room-size-"+id).addClass("hide");
             $("#room-input-name-"+id).removeClass("hide");
             $("#room-input-size-"+id).removeClass("hide");
+            $("#room-edit-button-"+id).addClass("hide");
+            $("#room-cancel-button-"+id).removeClass("hide");
+        }
+        function roomCancel(id){
+            //var name = $("#room-input-name-"+id).value;
+            var name = document.getElementById("room-name-"+id).innerText;
+            document.getElementById("room-input-name-box-"+id).value = name;
+            var size = document.getElementById("room-size-"+id).innerText;
+            size = size.split(" ");
+            size = size[0];
+            document.getElementById("room-input-size-box-"+id).value = size;
+            $("#room-name-"+id).removeClass("hide");
+            $("#room-size-"+id).removeClass("hide");
+            $("#room-input-name-"+id).addClass("hide");
+            $("#room-input-size-"+id).addClass("hide");
+            $("#room-edit-button-"+id).removeClass("hide");
+            $("#room-cancel-button-"+id).addClass("hide");
+        }
+        function roomRemove(id){
+            $("#confirm-remove-button").attr("onclick","confirmRemove("+id+")");
+        }
+        function confirmRemove(id){
+            $("#room-"+id).remove();
+        }
+        function roomCreate(){
+            var name = document.getElementById("room-input-name-new").value;
+            var size = document.getElementById("room-input-size-new").value;
+            document.getElementById("room-input-name-new").value = "";
+            document.getElementById("room-input-size-new").value = "";
+            room_count = room_count + 1;
+            var i = room_count;
+            var txt ='<tr id="room-'+i+'">'
+                    +'<td>'
+                    +'  <div id="room-name-'+i+'">'+name+'</div>'
+                    +'  <div id="room-input-name-'+i+'" class="hide">'
+                    +'      <input id="room-input-name-box-'+i+'" type="text" class="form-control" name="room['+i+'][]" placeholder="ชื่อห้องประชุม" value="'+name+'">'
+                    +'  </div>'
+                    +' </td>'
+                    +'<td>'
+                    +'  <div id="room-size-'+i+'">'+size+' คน</div>'
+                    +'  <div id="room-input-size-'+i+'" class="hide">'
+                    +'      <div class="col-xs-12 no-padding">'
+                    +'          <input id="room-input-size-box-'+i+'" type="text" class="form-control" style="display: inline; width: 90%;" name="room['+i+'][]" placeholder="จำนวนคนที่จุได้" value="'+size+'">'
+                    +'          คน'
+                    +'      </div>'
+                    +'  </div>'
+                    +'</td>'
+
+
+                    +'<td class="text-center">'
+                    +'  <a id="room-edit-button-'+i+'" onclick="roomEdit('+i+')" class="btn btn-3d btn-reveal btn-yellow">'
+                    +'      <i class="fa fa-edit"></i>'
+                    +'      <span>แก้ไข</span>'
+                    +'  </a>'
+                    +'  <a id="room-cancel-button-'+i+'" onclick="roomCancel('+i+')" class="btn btn-3d btn-reveal btn-red hide">'
+                    +'      <i class="fa fa-times"></i>'
+                    +'      <span>ยกเลิก</span>'
+                    +'  </a>'
+                    +'  <a id="room-remove-button-'+i+'" onclick="roomRemove('+i+')" class="delete-a-tuple social-icon social-icon-sm social-icon-round social-yelp" data-toggle="tooltip" data-placement="top" title="ลบห้องประชุมนี้" style="vertical-align:middle">'
+                    +'      <i class="fa fa-minus"></i>'
+                    +'      <i class="fa fa-trash" data-toggle="modal" data-target=".bs-example-modal-sm"></i>'
+                    +'  </a>'
+                    +'</td>'
+                    +'</tr>';
+            $(".room-table").append(txt);
         }
     </script>
 @endsection
