0f3c8a071af9900ce3a274dc7bc98d0b9eefe7f2
Kittiphop Phalakarn <Kittiphop Phalakarn>
2016-03-11 17:46:09 +0700

    search user dai laew na ei ei

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Permission;
 use Illuminate\Http\Request;
-
+use App\User;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -25,6 +25,18 @@ class StudentController extends Controller
     }
 
     public function search(Request $request){
-        return $request->input('data');
+        $user = User::select(['student_id','name','surname','nickname','sex','group','department','generation'])->where(function ($query) use ($request) {
+                   if($request->input('studentID')) $query->where('student_id', 'LIKE', '%'.$request->input('studentID').'%');
+                   if($request->input('studentFName')) $query->where('name', 'LIKE', '%'.$request->input('studentFName').'%');
+                   if($request->input('studentLName')) $query->where('surname', 'LIKE', '%'.$request->input('studentLName').'%');
+                   //if($request->input('')) $query->where('student_id', '=', '');
+                   //if($request->input('')) $query->where('student_id', '=', '');
+                   //if($request->input('')) $query->where('student_id', '=', '');
+                   //if($request->input('')) $query->where('student_id', '=', '');
+                   //if($request->input('')) $query->where('student_id', '=', '');
+                   //if($request->input('')) $query->where('student_id', '=', '');
+                   //if($request->input('')) $query->where('student_id', '=', '');
+                })->get();
+        return $user;
     }
 }
