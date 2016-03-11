91e9687ae01337724d8389554ede7f4c3cd7ccb0
Patipol Chiammunchit <Patipol Chiammunchit>
2016-03-11 12:58:08 +0700

    no message

--- resources/views/create-activity.blade.php
+++ resources/views/create-activity.blade.php
@@ -27,11 +27,11 @@
                                 <input name="activity_name" value="" class="form-control required"
                                        type="text">
                             </div>
-                            <div class="col-md-4 col-sm-4">
-                                <label>สถานะ *</label>
-                                <input name="activity_status" value="" class="form-control required"
-                                       type="text">
-                            </div>
+                            {{--<div class="col-md-4 col-sm-4">--}}
+                                {{--<label>สถานะ *</label>--}}
+                                {{--<input name="activity_status" value="" class="form-control required"--}}
+                                       {{--type="text">--}}
+                            {{--</div>--}}
                         </div>
                         <div class = "row">
                             <div class="col-md-8 col-sm-8">
@@ -99,13 +99,13 @@
                                 </label>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-8">
-                                <label>ไฟล์ *</label>
-                                <input class="custom-file-upload" type="file" id="file" name="file" id="file" data-btn-text="Select a File" />
+                        {{--<div class="row">--}}
+                            {{--<div class="col-md-8">--}}
+                                {{--<label>ไฟล์ *</label>--}}
+                                {{--<input class="custom-file-upload" type="file" id="file" name="file" id="file" data-btn-text="Select a File" />--}}
                                 {{--<small class="text-muted block">Max file size: 10Mb (zip/pdf/jpg/png)</small>--}}
-                            </div>
-                        </div>
+                            {{--</div>--}}
+                        {{--</div>--}}
                         <div class = "row">
                             <div class="col-md-2 col-sm-2">
                                 <label>ชั้นปีสุดท้ายที่เห็น *</label>
@@ -113,6 +113,53 @@
                                        type="text">
                             </div>
                         </div>
+                    </form>
+                    <form novalidate="novalidate" class="validate" action="{{url().'/setting/edit_permission'}}" method="post" enctype="multipart/form-data" data-error="เกิดความผิดพลาด กรุณาลองใหม่อีกครั้ง" data-success="เปลี่ยนแปลงสิทธิ์สำเร็จ" data-toastr-position="top-right">
+                        <fieldset>
+                            <input type="hidden" name="_token" value="{{{ csrf_token() }}}">
+                            <div class="row">
+                                <div class="form-group">
+                                    <div class="col-md-6 col-sm-6">
+                                        <label class="margin-bottom-20 ">เพิ่มผู้จัดการข้อมูล</label>
+                                        <div class="input-group autosuggest" data-minLength="1" data-queryURL="{{url('setting/auto_suggest?limit=10&search=1')}}">
+                                            <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                            <input id="studentInfo" name="studentInfo" class="form-control typeahead" placeholder="กรอกรหัสนิสิต/ชื่อ/นามสกุล" type="text">
+                                            <span class="input-group-btn" id="add-new-permission-btn">
+                                                <a class="btn btn-success">เพิ่ม</a>
+                                            </span>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <div class="table-responsive margin-bottom-30" style="float:left; width:50%">
+                            <table class="table nomargin" id="permission-table" width="100%">
+                                <tr>
+                                    <th style="vertical-align:middle" rowspan="2">รหัสนิสิต</th>
+                                    <th style="vertical-align:middle" rowspan="2">ชื่อ</th>
+                                    <th style="vertical-align:middle" rowspan="2">นามสกุล</th>
+                                </tr>
+                            </table>
+                        </div>
+
+                        {{--<div class="row">--}}
+                            {{--<div class="col-md-1">--}}
+                                {{--<button type="submit" class="btn btn-3d btn-reveal btn-green">--}}
+                                    {{--<i class="fa fa-check"></i>--}}
+                                    {{--<span>บันทึก</span>--}}
+                                {{--</button>--}}
+                            {{--</div>--}}
+                            {{--<div class="col-md-1 text-center">--}}
+                                {{--<span class="loading-icon"></span>--}}
+                            {{--</div>--}}
+                            {{--<div class="col-md-1">--}}
+                                {{--<a id="cancelPermissionEditButton" class="btn btn-3d btn-reveal btn-red">--}}
+                                    {{--<i class="fa fa-times"></i>--}}
+                                    {{--<span>ยกเลิก</span>--}}
+                                {{--</a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
                     </form>
                 </div>
             </div>
