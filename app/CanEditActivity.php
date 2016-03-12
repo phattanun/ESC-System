7708e40c31ac13e0f601169fc767d2db9e3ba48e
Patipol Chiammunchit <Patipol Chiammunchit>
2016-03-12 23:39:55 +0700

    Create activity 95%

--- app/CanEditActivity.php
+++ app/CanEditActivity.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class CanEditActivity extends Model
 {
     protected $table = "can_edit_activities";
+    protected $fillable = ['act_id','student_id'];
+    public $timestamps = false;
 }
