01b34ce3ec65186736acbb2b134fdd64c4405af1
Patipol Chiammunchit <Patipol Chiammunchit>
2016-03-19 11:02:48 +0700

    getting start to build the activity report page

---
+++ resources/views/activity-report.blade.php
@@ -0,0 +1,17 @@
+@extends('masterpage')
+
+@section('title')
+    รายงานกิจกรรม
+@endsection
+
+@section('body-attribute')
+    style ="background-color:#FCFCFC;"
+@endsection
+
+@section('bodyTitle')
+    รายงานกิจกรรม
+@endsection
+
+@section('content')
+
+@endsection
