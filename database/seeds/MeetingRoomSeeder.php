e4bf77e41560ed90e7f50182999deee315c74eb0
Krit <ac127.44264@gmail.com>
2016-03-25 02:47:45 +0700

    meeting room no id 0

--- database/seeds/MeetingRoomSeeder.php
+++ database/seeds/MeetingRoomSeeder.php
@@ -12,38 +12,38 @@ class MeetingRoomSeeder extends Seeder
     public function run()
     {
         MeetingRoom::create([
-            'room_id'=>0,
+            'room_id'=>1,
             'name'=>'ไม่ระบุห้อง',
             'size'=>'ไม่จำกัด',
             'priority'=>0
         ]);
         MeetingRoom::create([
-            'room_id'=>1,
+            'room_id'=>2,
             'name'=>'ห้องเขียว',
             'size'=>'ไม่จำกัด',
             'priority'=>1
         ]);
         MeetingRoom::create([
-            'room_id'=>2,
+            'room_id'=>3,
             'name'=>'ห้องชมพู',
             'size'=>'ใหญ่กว่าห้องเขียว',
             'priority'=>2
         ]);
         MeetingRoom::create([
-            'room_id'=>3,
+            'room_id'=>4,
             'name'=>'ห้องเก็บของ',
             'size'=>'เล็กมาก',
             'priority'=>3,
             'closed'=>true
         ]);
         MeetingRoom::create([
-            'room_id'=>4,
+            'room_id'=>5,
             'name'=>'ห้องสวีท',
             'size'=>'1-3 คน',
             'priority'=>3
         ]);
         MeetingRoom::create([
-            'room_id'=>5,
+            'room_id'=>6,
             'name'=>'ห้องประชุม',
             'size'=>'ใหญ่มาก',
             'priority'=>1
