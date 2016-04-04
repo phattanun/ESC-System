a3bdb1a62344cb98414faf9ce5c585b2e288432d
Mongkud Klungpornkun <mongkud167@gmail.com>
2016-04-04 18:28:36 +0700

    Add modal and Fork script from room-approve

--- resources/views/supplies-approve.blade.php
+++ resources/views/supplies-approve.blade.php
@@ -34,7 +34,7 @@
                                 <td style="vertical-align:middle;text-align: center">ชมรมค่ายลานเกียร์</td>
                                 <td style="vertical-align:middle;text-align: center">นายปฏิพล เจียมมั่นจิต</td>
                                 <td style="vertical-align:middle;text-align: center">
-                                    <button type="button" class="btn btn-3d btn-reveal btn-yellow" data-toggle="modal" data-target=".act_detail">
+                                    <button type="button" class="btn btn-3d btn-reveal btn-yellow" data-toggle="modal" data-target="#act-detail" onclick="loadData('id')">
                                         <i class="fa fa-edit"></i>
                                         <span>แก้ไข</span>
                                     </button>
@@ -46,6 +46,24 @@
             </div>
         </div>
     </section>
+    <div id="act-detail" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div id="header" class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 id="title" class="modal-title">Title</h4>
+          </div>
+          <div id="content" class="modal-body">
+            <p id="detail">Content<p>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
 @endsection
 
 @section('css')
@@ -53,4 +71,28 @@
 @endsection
 
 @section('js')
+    <script type="text/javascript">
+        function replace(data, postCallback) {
+            var modal = $("#act-detail");
+            console.log(data);
+
+            var infoTabs = Object.getOwnPropertyNames(data);
+            for(i in infoTabs) {
+                var names = Object.getOwnPropertyNames(data[infoTabs[i]]);
+                for(j in names) {
+                    //console.log("#"+infoTabs[i]+" *[id="+names[j]+"]");
+                    modal.find("#"+infoTabs[i]+" *[id="+names[j]+"]:not(input)").html(data[infoTabs[i]][names[j]]);
+                    modal.find("#"+infoTabs[i]+" input[id="+names[j]+"]").val(data[infoTabs[i]][names[j]]);
+                    if(postCallback != null)
+                        postCallback(names[j],modal.find("#"+infoTabs[i]+" *[id="+names[j]+"]"),data[infoTabs[i]][names[j]]);
+                }
+            }
+        }
+        function loadData(id) {
+            replace({
+                'header' : { 'title': 'Replaced Title'},
+                'content' : { 'detail' : 'Replaced Content' }
+            });
+        }
+    </script>
 @endsection
