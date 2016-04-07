36ca27515d628ea034227ed59c34b295c9a2f8be
kittyphon <kittyphon@gmail.com>
2016-04-07 18:27:12 +0700

    edit supplier seed

--- database/seeds/SupplierSeeder.php
+++ database/seeds/SupplierSeeder.php
@@ -13,14 +13,14 @@ class SupplierSeeder extends Seeder
     public function run()
     {
         Supplier::create([
+            'supplier_id'=>'1',
             'name'=>'จีฉ่อย',
-            'inv_id'=>'1',
             'address'=>'32/25 ถนนพระราม4 แขวงปทุมวัน เขตพญาไท กรุงเทพฯ 10111',
             'phone_no'=>'085-555-5566'
         ]);
         Supplier::create([
+            'supplier_id'=>'2',
             'name'=>'สมใจ',
-            'inv_id'=>'2',
             'address'=>'45/23-5 ถนนพญาไท แขวงพญาไท เขตพญาไท กรุงเทพฯ 11000',
             'phone_no'=>'085-656-6666'
         ]);
