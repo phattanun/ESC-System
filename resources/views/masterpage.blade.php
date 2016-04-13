287c810e895fd48089f3522472e3a6fbae342368
Phattanun Aukkarapuntouch <Phattanun Aukkarapuntouch>
2016-04-13 21:01:52 +0700

    edit masterpage

--- resources/views/masterpage.blade.php
+++ resources/views/masterpage.blade.php
@@ -161,7 +161,6 @@
                                     @if(isset($user['supplies']))<li><a href="{{ URL::to('/supplies/approve') }}">อนุมัติการยืมพัสดุ</a></li>@endif
                                     @if(isset($user['supplies']))<li><a href="{{ URL::to('/supplies/manage') }}">จัดการยืม-คืนพัสดุ</a></li>@endif
                                     <li><a href="{{ URL::to('/supplies/search') }}">ประวัติการยืม</a></li>
-                                    @if(isset($user['supplies']))<li><a href="">แก้ไขข้อมูลพัสดุ</a></li>@endif
                                     @if(isset($user['supplies']))<li><a href="{{ URL::to('supplies/supplier') }}">แก้ไขข้อมูลร้านค้า</a></li>@endif
                                     @if(isset($user['supplies']))<li><a href="{{ URL::to('/supplies/report') }}">ออกรายงาน</a></li>@endif
                                 </ul>
